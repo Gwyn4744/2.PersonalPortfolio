dd259ee82281de6538831aaab782eb5003fd6cab
Dominik Bednarski <dominikbednarski@wp.pl>
2020-10-01 13:01:15 +0200

    Modified the pages. Added filtering and modified admin object views v0.0.8

--- personal_portfolio/blog/views.py
+++ personal_portfolio/blog/views.py
@@ -4,8 +4,9 @@ from .models import Blog
 # Create your views here.
 
 def all_blogs(request):
+    blogs_count = Blog.objects.count
     blogs = Blog.objects.order_by('-date')[:5]
-    return render(request, 'blog/blog.html', {'blogs': blogs})
+    return render(request, 'blog/blog.html', {'blogs': blogs, 'blogs_count': blogs_count})
 
 def detail(request, blog_id):
     blog = get_object_or_404(Blog, pk=blog_id)
