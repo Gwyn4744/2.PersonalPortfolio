7080a03116daed9b089a6ba5ebf9c8921905bf03
Dominik Bednarski <dominikbednarski@wp.pl>
2020-10-03 02:14:25 +0200

    Modificated style v0.0.11

--- personal_portfolio/portfolio/views.py
+++ personal_portfolio/portfolio/views.py
@@ -4,5 +4,5 @@ from .models import Project
 # Create your views here.
 
 def home(request):
-    projects = Project.objects.all()
+    projects = Project.objects.order_by('-pk')[:5]
     return render (request, 'portfolio/home.html', {'projects':projects})
